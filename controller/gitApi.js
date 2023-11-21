ac06caa925c4b73c0b7e48d49569700e3654c6a6
RootPark <rmsdnjs518@gmail.com>
2023-11-21 22:03:30 +0900

    FEAT) cron automation

--- controller/gitApi.js
+++ controller/gitApi.js
@@ -120,4 +120,75 @@ const postDateCommits = async (req, res) => {
     timeLog('[POST] date-commits called // ' + JSON.stringify(req.body) + '//' + JSON.stringify(results));
 };
 
-export { postUserInfo, postDateCommits };
+
+// ----------- postDateCommits --------------
+const postDateCommitsAuto = async (year, month, date) => {
+
+    const fullDate = year + "-" + month + "-" + date;
+
+    const baseUrl = "https://api.github.com/users/";
+    const eventUrl = "/events";
+
+    const results = {};
+    results.result = true;
+    results.error = [];
+    results.commits = []; // Initialize commits array
+
+    try {
+        await getAllUsers(results);
+    } catch (err) {
+        results.result = false;
+        results.error.push('getAllUsers Error');
+    }
+
+    try {
+        for (let i = 0; i < results.users.length; i++) {
+            const name = results.users[i].name;
+            const url = baseUrl + name + eventUrl;
+            try {
+                const response = await axios.get(url, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                const commits = response.data;
+
+                const specificCommits = commits.filter(commit => {
+                    return commit.created_at.includes(fullDate);
+                });
+
+                try {
+                    for (let j = 0; j < specificCommits.length; j++) {
+                        const Id = specificCommits[j].id;
+                        const repo = specificCommits[j].repo.name;
+                        const created = moment(specificCommits[j].created_at).add(9, 'hours').toISOString();
+
+                        try {
+                            await writeCommits(Id, name, repo, created, results);
+                        } catch (err) {
+                            results.result = false;
+                            results.error.push('writeCommits Error : ' + err.message);
+                        }
+
+                    }
+                } catch (err) {
+                    results.result = false;
+                    results.error.push('Error findout commits : ' + err.message);
+                }
+            } catch (err) {
+                results.result = false;
+                results.error.push('Error fetching commits for user ' + name);
+            }
+        }
+    } catch (err) {
+        results.result = false;
+        results.error.push('Git Api Error : ' + err.message);
+    }
+
+    res.send(results);
+    consoleBar();
+    timeLog('[POST] date-commits called // ' + JSON.stringify(req.body) + '//' + JSON.stringify(results));
+};
+
+export { postUserInfo, postDateCommits, postDateCommitsAuto };
--- green-check.js
+++ green-check.js
@@ -5,12 +5,13 @@ dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import cron from 'node-cron';
 
 import config from './config/config.js';
 import { consoleBar, timeLog } from './config/common.js';
 import { ping } from './controller/system.js';
 import { getCommits, getDateCommits, getUsers, getuserCommits, getuserInfo } from './lib/db.js';
-import { postDateCommits, postUserInfo } from './controller/gitApi.js';
+import { postDateCommits, postDateCommitsAuto, postUserInfo } from './controller/gitApi.js';
 
 // ------------------ router set -----------------
 
@@ -31,6 +32,18 @@ router.route('/user-commits').get(getuserCommits);
 router.route('/date-commits').get(getDateCommits);
 router.route('/user-info').post(postUserInfo);
 router.route('/date-commits').post(postDateCommits);
+
+// ---------------- date automation -----------------
+
+cron.schedule('0 0 * * *', () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const date = currentDate.getDate();
+
+    postDateCommitsAuto(year, month, date);
+});
+
 
 // ---------------- server start -----------------
 
